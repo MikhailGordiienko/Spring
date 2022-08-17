0bea12ba745c9bb958d40019128cb29432a53173
Mikhail Hordiienko <mikhailgordiyenko1994@gmail.com>
2022-08-18 01:15:42 +0300

    manual dependency injection

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -5,9 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "AplicationContext.xml"
+                "ApplicationContext.xml"
         );
-        TestBean bean = context.getBean("testBean", TestBean.class);
-        System.out.println(bean.getName());
+        Music music = context.getBean("musicBean", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
     }
 }
